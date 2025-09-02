1b470d1286e77d48820ba84be225420a33514bf3
Sean Corfield <sean@corfield.org>
2025-09-02 12:56:40 -0400

    update to nrepl 1.4.0
    and fix reflection warnings
    
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- src/org/corfield/dev/repl.clj
+++ src/org/corfield/dev/repl.clj
@@ -1,77 +1,77 @@
 ;; copyright (c) 2018-2025 sean corfield, all rights reserved
 
- (ns org.corfield.dev.repl
-   "Invoke org.corfield.dev.repl/-main to start a REPL based on
+(ns org.corfield.dev.repl
+  "Invoke org.corfield.dev.repl/-main to start a REPL based on
   what tooling you have available on your classpath."
-   (:require [clojure.repl :refer [demunge]]
-             [clojure.string :as str]))
+  (:require [clojure.repl :refer [demunge]]
+            [clojure.string :as str]))
 
- (when-not (resolve 'requiring-resolve)
-   (throw (ex-info ":dev/repl and repl.clj require at least Clojure 1.10"
-                   *clojure-version*)))
+(when-not (resolve 'requiring-resolve)
+  (throw (ex-info ":dev/repl and repl.clj require at least Clojure 1.10"
+                  *clojure-version*)))
 
- (defn- socket-repl-port
-   "Return truthy if it looks like a Socket REPL Server is wanted,
+(defn- socket-repl-port
+  "Return truthy if it looks like a Socket REPL Server is wanted,
   else return nil. The truthy value is the port number to use.
 
   Checks the following for port numbers:
   * SOCKET_REPL_PORT env var - a value of none suppresses it
   * socket-repl-port property - a value of none suppresses it
   * .socket-repl-port file - assumed to contain a port number"
-   []
-   (let [s-port (or (System/getenv "SOCKET_REPL_PORT")
-                    (System/getProperty "socket-repl-port")
-                    (try (slurp ".socket-repl-port") (catch Throwable _)))]
-     (when-not (= "none" s-port)
-       (try
-         (Long/parseLong s-port)
-         (catch Throwable _)))))
+  []
+  (let [s-port (or (System/getenv "SOCKET_REPL_PORT")
+                   (System/getProperty "socket-repl-port")
+                   (try (slurp ".socket-repl-port") (catch Throwable _)))]
+    (when-not (= "none" s-port)
+      (try
+        (Long/parseLong s-port)
+        (catch Throwable _)))))
 
- (defn- tap>log [frame level throwable message]
-   (let [class-name (symbol (demunge (.getClassName frame)))]
+(defn- tap>log [^StackTraceElement frame level throwable message]
+  (let [class-name (symbol (demunge (.getClassName frame)))]
     ;; only called for enabled log levels:
-     (tap>
-      (with-meta
-        {:form     '()
-         :level    level
-         :result   (or throwable message)
-         :ns       (symbol (or (namespace class-name)
+    (tap>
+     (with-meta
+       {:form     '()
+        :level    level
+        :result   (or throwable message)
+        :ns       (symbol (or (namespace class-name)
                                                                         ;; fully-qualified classname - strip class:
-                               (str/replace (name class-name) #"\.[^\.]*$" "")))
-         :file     (.getFileName frame)
-         :line     (.getLineNumber frame)
-         :column   0
-         :time     (java.util.Date.)
-         :runtime  :clj}
-        {:dev.repl/logging true}))))
+                              (str/replace (name class-name) #"\.[^\.]*$" "")))
+        :file     (.getFileName frame)
+        :line     (.getLineNumber frame)
+        :column   0
+        :time     (java.util.Date.)
+        :runtime  :clj}
+       {:dev.repl/logging true}))))
 
- (defn- ctl-log*adapter [log-star]
-   (let [log*-fn (deref log-star)]
-     (fn [logger level throwable message]
-       (try
-         (let [^StackTraceElement frame (nth (.getStackTrace (Throwable. "")) 2)]
-           (tap>log frame level throwable message))
-         (catch Throwable _))
-       (log*-fn logger level throwable message))))
+(defn- ctl-log*adapter [log-star]
+  (let [log*-fn (deref log-star)]
+    (fn [logger level throwable message]
+      (try
+        (let [frame (nth (.getStackTrace (Throwable. "")) 2)]
+          (tap>log frame level throwable message))
+        (catch Throwable _))
+      (log*-fn logger level throwable message))))
 
- (defn- logging4j2*adapter [log-star]
-   (let [log*-fn (deref log-star)]
-     (fn [logger level & more]
-       (let [[throwable message-parts]
+(defn- logging4j2*adapter [log-star]
+  (let [log*-fn (deref log-star)]
+    (fn [logger level & more]
+      (let [[throwable message-parts]
             ;; drop marker if present; find throwable and everything else:
-             (cond (instance? Throwable (first more))
-                   [(first more) (rest more)]
-                   (instance? Throwable (second more))
-                   [(second more) (rest (rest more))]
-                   :else
-                   [nil more])
-             message (str/join " " message-parts)]
-         (try
-           (let [^StackTraceElement frame (nth (.getStackTrace (Throwable. "")) 3)]
-             (tap>log frame (-> level str str/lower-case keyword)
-                      throwable message))
-           (catch Throwable _))
-         (apply log*-fn logger level more)))))
+            (cond (instance? Throwable (first more))
+                  [(first more) (rest more)]
+                  (instance? Throwable (second more))
+                  [(second more) (rest (rest more))]
+                  :else
+                  [nil more])
+            message (str/join " " message-parts)]
+        (try
+          (let [frame (nth (.getStackTrace (Throwable. "")) 3)]
+            (tap>log frame (-> level str str/lower-case keyword)
+                     throwable message))
+          (catch Throwable _))
+        (apply log*-fn logger level more)))))
 
 (defn -main
   "If Jedi Time is on the classpath, require it (so that Java Time
@@ -114,7 +114,7 @@
           ((requiring-resolve 'clojure.core.server/start-server)
            {:port s-port :name server-name
             :accept 'clojure.core.server/repl})
-          (let [s-port' (.getLocalPort
+          (let [s-port' (.getLocalPort ^java.net.ServerSocket
                          (get-in @(requiring-resolve 'clojure.core.server/servers)
                                  [server-name :socket]))]
             (println "Selected port" s-port' "for the Socket REPL...")
@@ -206,7 +206,9 @@
   (-> (java.lang.management.ManagementFactory/getRuntimeMXBean)
       (.getUptime)
       (java.time.Duration/ofMillis)
-      (as-> t (map #(% t) [#(.toHours %) #(.toMinutesPart %) #(.toSecondsPart %)])
+      (as-> t (map #(% t) [#(.toHours       ^java.time.Duration %)
+                           #(.toMinutesPart ^java.time.Duration %)
+                           #(.toSecondsPart ^java.time.Duration %)])
         (let [[h & ms] t]
           (map vector
                (into ((juxt #(long (/ % 24)) #(mod % 24)) h) ms)
